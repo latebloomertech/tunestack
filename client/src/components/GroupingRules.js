91e0dae82daec3fdfc195561ae0d01a93dde64df
Dan Baechtold <latebloomertech@gmail.com>
2022-04-07 14:21:46 -0400

    User settings flow cards

--- client/src/components/GroupingRules.js
+++ client/src/components/GroupingRules.js
@@ -4,12 +4,82 @@ import { Link } from "react-router-dom"
 
 function GroupingRules() {
     return (
-        <div>GroupingRules
+        <div className='flow-card'>
+            <h2 className='flow-card-title'>How do you want to group your new playlists?</h2>
+
+
+            <div className='flow-card-choices-container'>
+            <p>Group my songs into playlists according to:</p>
+
+                <form className='flow-card-form'>
+                    <div className='radio'>
+                        <input
+                            type="radio"
+                            id="testID"
+                            name="testName"
+                            value="placeholder" />
+                        <label> The year I saved the song</label>
+                    </div>
+
+                    <div className='radio'>
+                    <input
+                        type="radio"
+                        id="testID"
+                        name="testName"
+                        value="placeholder" />
+                    <label> The month I saved the song</label>
+                    </div>
+
+                    <div className='radio'>
+                    <input
+                        type="radio"
+                        id="testID"
+                        name="testName"
+                        value="placeholder" />
+                    <label> The week I saved the song </label>
+                    </div>
+
+                    <div className='radio'>
+                    <input
+                        type="radio"
+                        id="testID"
+                        name="testName"
+                        value="placeholder" />
+                    <label> The energy level of the song</label>
+                    </div>
+
+                    <div className='radio'>
+                    <input
+                        type="radio"
+                        id="testID"
+                        name="testName"
+                        value="placeholder" />
+                    <label> The instrumentalness of the song</label>
+                    </div>
+
+                    <div className='radio'>
+                    <input
+                        type="radio"
+                        id="testID"
+                        name="testName"
+                        value="placeholder" />
+                    <label>The popularity of the song</label>
+                    </div>
+
+                </form>
+            </div>
+
+
+
+            {/* <br></br> */}
+            <div className='flow-card-next'>
             <Link to={`/rules/filtering`}>
-                <button>
+                <button className='flow-card-button'>
                     Next
                 </button>
             </Link>
+            </div>
+
         </div>
     )
 }
