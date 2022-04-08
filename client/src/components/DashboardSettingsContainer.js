0d899f19e2d95b26690859364e660f7e65c9f0a4
Nic Byron <nicjbyron@gmail.com>
2022-04-08 12:06:33 -0400

    Static settings components on Dashboard

---
+++ client/src/components/DashboardSettingsContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function DashboardSettingsContainer({children}) {
+
+    return (
+        <div className='content-block content-block-tertiary'>
+            {children}
+        </div>
+    )
+}
+
+export default DashboardSettingsContainer
