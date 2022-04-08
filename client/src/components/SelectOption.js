205b0a1950893a319cba182fbbea235b4d4e8bec
Nic Byron <nicjbyron@gmail.com>
2022-04-08 16:41:03 -0400

    Minor styling adjustments focusing on the width of the overall components

--- client/src/components/SelectOption.js
+++ client/src/components/SelectOption.js
@@ -8,7 +8,9 @@ function SelectOption({ children, handleSelect, selectedOption, value, label }) 
     function isSelected() {
         return selectedOption == value
     }
-    return (<button value={value} className={`content-block content-block-tertiary content-block-select-option ${isSelected() ? 'content-block-select-option-selected' : ''}`} onClick={handleClick}>
+    return (<button value={value}
+        className={`content-block content-block-tertiary content-block-select-option ${isSelected() ? 'content-block-select-option-selected' : ''}`}
+        onClick={handleClick}>
         <h4>{label}</h4>
         <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Simple_Music.svg/1200px-Simple_Music.svg.png?20110615130507' className='select-option-image'></img>
     </button>
